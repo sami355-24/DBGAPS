b5038b023df2eb12b9d6da4e003e6c99dde039b7
sami355 <ddung355@gmail.com>
2022-06-25 21:29:19 +0900

    after doing this =============================== - run on cloud server - setting ip and port - ip : 49.50.166.163 - port : 9999

---
+++ src/main/java/Nogosan/DBGAPS/DbgapsApplication.java
@@ -0,0 +1,16 @@
+package Nogosan.DBGAPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class DbgapsApplication {
+
+	public static void main(String[] args)  {
+		SpringApplication.run(DbgapsApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/Nogosan/DBGAPS/DbgapsApplicationTests.java
@@ -0,0 +1,13 @@
+package Nogosan.DBGAPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbgapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
